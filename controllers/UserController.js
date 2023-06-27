38265db5d86c12c4d259a62731bf1431de5fab48
Eliezer Assuncao <eliezerassuncaocustodio@gmail.com>
2023-06-26 23:49:03 -0300

    getting profile_ photo from database

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -46,7 +46,7 @@ class UserController {
           nome: decoded.nome,
           isPremium: decoded.isPremium,
           username: decoded.username,
-          profilePhoto: decoded.profile_photo,
+          profilePhoto: decoded.profilePhoto,
         });
       } catch (err) {
         res.status(403);
